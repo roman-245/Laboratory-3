2ca81eaa79cf7d3eff145e7455e122ea905c2bdf
Petr <peter30260@gmail.com>
2024-04-12 11:41:44 +0300

    Added all functions and handle_commands_function

--- Server_lab3.py
+++ Server_lab3.py
@@ -12,6 +12,13 @@ server_socket.listen(5)
 
 def handle_commands(conn):
     data = conn.recv(1024)
+    #Тимофей
+    if data.decode() == 'tim':
+        f1 = input('Введите путь к корневой папке: ')
+        f2 = input('Введите путь к папке синхронизации: ')
+        check_interval = 5
+        sync_folders(f1, f2, check_interval)
+    #Пётр
     elif data.decode() == "petr":
         folders = []
         numbers = []
@@ -21,6 +28,92 @@ def handle_commands(conn):
         treemaker(numbers, today)
         conn.close()
         print('Соединение разорвано\n')
+    #Анастасия
+    elif data.decode() == 'stasy':
+        files = received_files(conn)
+        directory1 = f'{os.getcwd()}/a'
+        directory2 = f'{os.getcwd()}/b'
+        synchronize_folders(directory1, directory2, 5, files)
+    #Иван
+    elif data.decode() == "var5":
+        commands = []
+        while True:
+            try:
+                command = conn.recv(1024).decode()
+                if command == "var5_add_command":
+                    new_command = conn.recv(1024).decode()
+                    commands.append(new_command)
+                    print(commands)
+                    run(commands)
+                elif command == "var5_get_file":
+                    program_name = conn.recv(1024).decode()
+                    send_files(conn, program_name)
+            except ConnectionResetError:
+                print("Подключение оборвалось")
+                break
+            except Exception as e:
+                print(f"Error: {e}")
+                break
+    #Роман
+    elif data.decode() == "var1":  # Пример ввода варианта - var1
+        while True:
+
+            data1 = conn.recv(1024)
+            data2 = conn.recv(1024)
+
+            if not data1 or not data2:
+                break
+
+            if str(data1.decode()) == "json" or str(data1.decode()) == "xml":
+
+                # Создание директории
+                directory_name = create_directory()
+
+                # Расширение для файлов
+                file_extension = str(data1.decode())
+
+                # Распаковываем данные обратно в список
+                numbers = struct.unpack(f'{len(data2) // 4}i', data2)
+
+                for number in numbers:
+                    save_data(number, file_extension, directory_name)
+
+                # Строим бинарное дерево
+                root_node = build_binary_tree(numbers)
+
+                # Преобразуем дерево в JSON
+                json_tree = convert_tree_to_json(root_node)
+
+                # Сохраняем JSON в файл
+                with open(f"{directory_name}/binary_tree.json", "w") as file:
+                    json.dump(json_tree, file, indent=4)
+
+                print("Бинарное дерево успешно сохранено в файл 'binary_tree.json'")
+
+            else:
+                directory = str(data1.decode())
+                all_directory = [filename for filename in os.listdir()]
+
+                if directory in all_directory:
+                    file_name = str(data2.decode()) + ".json"
+
+                    if file_name in os.listdir(directory):
+                        path_to_file = directory + '/' + file_name
+
+                        with open(path_to_file, 'r') as json_file:
+                            data = json.load(json_file)
+                            string = json.dumps(data)
+                            conn.send(struct.pack(f'I{len(string)}s', len(string), string.encode()))
+
+                    else:
+                        print("Такой файл не существует!")
+                        conn.close()
+
+                else:
+                    print("Такой директории не существует!")
+                    conn.close()
+
+
     else:
         print('введите существующий вариант')
 
--- all_function_server.py
+++ all_function_server.py
@@ -1,5 +1,15 @@
 import os
 import json
+import struct
+import pickle
+import shutil
+import time
+import subprocess
+import datetime
+import xml.etree.ElementTree as ET
+
+
+# Функции Петра
 def treemaker(numbers, today):
     try:
         numbers[0] = Branch(numbers[0])
@@ -83,3 +93,222 @@ def json_sender(folders, index, number, conn):
                 sent += conn.send(bytes((data[sent:sent + chunk_size]), encoding="utf-8"))
     except FileNotFoundError:
         conn.sendall('Указанный файл не найден'.encode())
+
+
+# Функции Анастасии
+def synchronize_folders(directory1, directory2, gap, files):
+    while True:
+        add_files = files - set(os.listdir(directory2))
+        remove_files = set(os.listdir(directory2)) - files
+        for file in add_files:
+            start = os.path.join(directory1, file)
+            finish = os.path.join(directory2, file)
+            shutil.copy(start, finish)
+            print('Файл', file, 'был добавлен')
+        for file in remove_files:
+            start = os.path.join(directory2, file)
+            os.remove(start)
+            print('Файл', file, 'был удален')
+        time.sleep(gap)
+
+
+def received_files(conn):
+    # Receive the size of the data and decode it
+    res = conn.recv(4)
+    data_size, = struct.unpack('!I', res)
+    received_data = b''
+    while len(received_data) < data_size:
+        received_data += conn.recv(1024)
+    files = pickle.loads(received_data)
+    print("Полученные данные от клиента:", files)
+    return files
+
+
+# Функции Ивана
+
+
+def run(commands):
+    make_dir(commands)
+    for command in commands:
+        timestamp = time.strftime("%d-%m_%H-%M-%S")
+        file_name = os.getcwd() + "/" + command.split(" ")[0] + "/" + f"{timestamp}_output.txt"
+        with open(file_name, 'a') as f:
+            subprocess.run(command, shell=True, stdout=f)
+
+
+def make_dir(commands):
+    for command in commands:
+        directory_name = command.split(" ")
+        os.makedirs(directory_name[0], exist_ok=True)
+
+
+def send_files(conn, program_name):
+    path = os.getcwd() + "/" + program_name
+    if os.path.exists(path):
+        file_list = os.listdir(path)
+        for file in file_list:
+            with open(path + "/" + file, "rb") as f:
+                while True:
+                    file_data = f.readline()
+                    file_size = len(file_data)
+                    conn.send(struct.pack("!I", file_size))
+                    conn.send(file_data)
+                    if not file_data:
+                        break
+
+
+# Функции Романа
+
+def create_directory():
+    # Текущае время
+    now = datetime.datetime.now()
+    directory_name = f"{now:%d-%m-%Y_%H-%M-%S}"
+
+    # Создание директории
+    os.mkdir(directory_name)
+    return directory_name
+
+
+# Функция для создания файла и сохранения данных
+def save_data(number, file_extension, directory_name, file_name=None):
+    # Если имя файла было не задано аргументом, то присваиваем number как имя
+    if not file_name:
+        file_name = str(number)
+    file_path = directory_name + "/" + file_name + "." + file_extension
+
+    # Создание и заполнение данными файл
+    data = {"value": number}
+
+    if file_extension == "json":
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    elif file_extension == "xml":
+        root = ET.Element("data")
+        root.text = str(data)
+        tree = ET.ElementTree(root)
+        tree.write(file_path, encoding="utf-8", xml_declaration=True)
+
+
+# Класс для узлов в дереве
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+# Функция для создания файла, который представляет собой дерево
+def build_binary_tree(nums):
+    if not nums:
+        return None
+
+    root = Node(nums[0])
+    queue = [root]
+    i = 1
+
+    while i < len(nums):
+        # Полученние текущей ссылки для класса
+        current_node = queue.pop(0)
+
+        # Добавление слева
+        left_value = nums[i] if i < len(nums) else None
+        if left_value is not None:
+            current_node.left = Node(left_value)
+            queue.append(current_node.left)
+
+        i += 1
+
+        # Добавление справа
+        right_value = nums[i] if i < len(nums) else None
+        if right_value is not None:
+            current_node.right = Node(right_value)
+            queue.append(current_node.right)
+
+        i += 1
+
+    return root
+
+
+# Функция для получения данных с дерева рекурсивно
+def convert_tree_to_json(root):
+    if not root:
+        return None
+
+    tree_dict = {}
+    tree_dict['value'] = root.value
+    tree_dict['left'] = convert_tree_to_json(root.left)
+    tree_dict['right'] = convert_tree_to_json(root.right)
+
+    return tree_dict
+
+def synchronize_folders(directory1, directory2, gap, files):
+    while True:
+        add_files = files - set(os.listdir(directory2))
+        remove_files = set(os.listdir(directory2)) - files
+        for file in add_files:
+            start = os.path.join(directory1, file)
+            finish = os.path.join(directory2, file)
+            shutil.copy(start, finish)
+            print('Файл', file, 'был добавлен')
+        for file in remove_files:
+            start = os.path.join(directory2, file)
+            os.remove(start)
+            print('Файл', file, 'был удален')
+        time.sleep(gap)
+
+
+def create_directory():
+    # Текущае время
+    now = datetime.datetime.now()
+    directory_name = f"{now:%d-%m-%Y_%H-%M-%S}"
+
+    # Создание директории
+    os.mkdir(directory_name)
+    return directory_name
+
+
+def received_files(conn):
+    # Receive the size of the data and decode it
+    res = conn.recv(4)
+    data_size, = struct.unpack('!I', res)
+    received_data = b''
+    while len(received_data) < data_size:
+        received_data += conn.recv(1024)
+    files = pickle.loads(received_data)
+    print("Полученные данные от клиента:", files)
+    return files
+
+    # функции Тимофея
+
+
+
+def sync_folders(folder1, folder2, interval):
+    while True:
+        # Get the list of files in both folders
+        files1 = set(os.listdir(folder1))
+        files2 = set(os.listdir(folder2))
+
+        print(f'Содержимое папки сервера: {files1}')
+        print(f'Содержимое папки клиента: {files2}')
+
+        # Find files to be added or removed in folder2
+        to_add = files1 - files2
+        to_remove = files2 - files1
+
+        # Add missing files from folder1 to folder2
+        for file in to_add:
+            src = os.path.join(folder1, file)
+            dst = os.path.join(folder2, file)
+            shutil.copy(src, dst)
+            print(f"Added: {file}")
+
+        # Remove extra files from folder2
+        for file in to_remove:
+            src = os.path.join(folder2, file)
+            os.remove(src)
+            print(f"Removed: {file}")
+
+        time.sleep(interval)
+
+#Роман
